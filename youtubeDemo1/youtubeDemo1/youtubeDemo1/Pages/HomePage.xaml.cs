86f398276a5b5685b3d66d60602af8c80c06b850
phuc <phucquan0106@gmail.com>
2019-10-02 17:47:13 +0700

    sem3 uwp mix 2

--- youtubeDemo1/youtubeDemo1/youtubeDemo1/Pages/HomePage.xaml.cs
+++ youtubeDemo1/youtubeDemo1/youtubeDemo1/Pages/HomePage.xaml.cs
@@ -5,12 +5,14 @@ using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.System;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
 using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Media.Animation;
 using Windows.UI.Xaml.Navigation;
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
@@ -26,5 +28,107 @@ namespace youtubeDemo1.Pages
         {
             this.InitializeComponent();
         }
+
+        private readonly List<(string Tag, Type page)> _pages = new List<(string Tag, Type page)>
+        {
+            ("home",typeof(HomePage)),
+            
+        };
+
+        private void NavView_OnLoaded(object sender, RoutedEventArgs e)
+        {
+            //navView.MenuItems.Add(new NavigationViewItemSeparator());
+            //navView.MenuItems.Add(new NavigationViewItem
+            //{
+            //    Content = "My Content",
+            //    Icon = new SymbolIcon((Symbol)0xF1AD),
+            //    Tag = "content"
+            //});
+            //_pages.Add(("content", typeof(HomePage)));
+            //navView.SelectedItem = navView.MenuItems[0];
+
+            // You can also add items in code.
+            NavView.MenuItems.Add(new NavigationViewItemSeparator());
+            NavView.MenuItems.Add(new NavigationViewItem
+            {
+                Content = "My content",
+                Icon = new SymbolIcon((Symbol)0xF1AD),
+                Tag = "content"
+            });
+            _pages.Add(("content", typeof(Register)));
+
+            // Add handler for ContentFrame navigation.
+            ContentFrame.Navigated += On_Navigated;
+
+            // NavView doesn't load any page by default, so load home page.
+            NavView.SelectedItem = NavView.MenuItems[0];
+            // If navigation occurs on SelectionChanged, this isn't needed.
+            // Because we use ItemInvoked to navigate, we need to call Navigate
+            // here to load the home page.
+            NavView_Navigate("home", new EntranceNavigationTransitionInfo());
+
+            // Add keyboard accelerators for backwards navigation.
+            var goBack = new KeyboardAccelerator { Key = VirtualKey.GoBack };
+            goBack.Invoked += BackInvoked;
+            this.KeyboardAccelerators.Add(goBack);
+
+            // ALT routes here
+            var altLeft = new KeyboardAccelerator
+            {
+                Key = VirtualKey.Left,
+                Modifiers = VirtualKeyModifiers.Menu
+            };
+            altLeft.Invoked += BackInvoked;
+            this.KeyboardAccelerators.Add(altLeft);
+        }
+
+        private void BackInvoked(KeyboardAccelerator sender,
+            KeyboardAcceleratorInvokedEventArgs args)
+        {
+            On_BackRequested();
+            args.Handled = true;
+        }
+
+        private bool On_BackRequested()
+        {
+            if (!ContentFrame.CanGoBack)
+                return false;
+
+            // Don't go back if the nav pane is overlayed.
+            if (NavView.IsPaneOpen &&
+                (NavView.DisplayMode == NavigationViewDisplayMode.Compact ||
+                 NavView.DisplayMode == NavigationViewDisplayMode.Minimal))
+                return false;
+
+            ContentFrame.GoBack();
+            return true;
+        }
+
+        private void NavView_Navigate(string navItemTag, NavigationTransitionInfo transitionInfo)
+        {
+            
+        }
+
+        private void On_Navigated(object sender, NavigationEventArgs e)
+        {
+            
+        }
+
+        private void NavView_OnItemInvoked(NavigationView sender, NavigationViewItemInvokedEventArgs args)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void NavView_OnBackRequested(NavigationView sender, NavigationViewBackRequestedEventArgs args)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void ContentFrame_OnNavigationFailed(object sender, NavigationFailedEventArgs e)
+        {
+            throw new Exception("Failed to load page "+ e.SourcePageType.FullName);
+        }
+
+        
     }
 }
---
+++ youtubeDemo1/youtubeDemo1/youtubeDemo1/Pages/Register.xaml.cs
@@ -0,0 +1,71 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Data.Json;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using Newtonsoft.Json.Linq;
+using youtubeDemo1.Entities;
+
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace youtubeDemo1.Pages
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class Register : Page
+    {
+        private const string ApiUrl = "https://2-dot-backup-server-003.appspot.com/_api/v2/members";
+
+        public Register()
+        {
+            this.InitializeComponent();
+        }
+
+        private void BtnSubmit_OnClick(object sender, RoutedEventArgs e)
+        {
+        
+        var member = new Member
+            {
+                firstName = txtFirstName.Text,
+                lastName = txtLastName.Text,
+                password = txtPassword.Password.ToString(),
+                address = txtAddress.Text,
+                avatar = txtAvatar.Text,
+                birthday = txtBirthday.Text,
+                email = txtEmail.Text,
+                gender = Int32.Parse(txtGender.Text),
+                introduction = txtIntroduction.Text,
+                phone = txtPhone.Text
+            };
+
+            Debug.WriteLine(JsonConvert.SerializeObject(member));
+            var httpClient = new HttpClient();
+            HttpContent httpContent = new StringContent(JsonConvert.SerializeObject(member),
+                Encoding.UTF8,"application/json");
+            Task<HttpResponseMessage> httpRequestMessage = httpClient.PostAsync(ApiUrl, httpContent);
+            String responseContent = httpClient.PostAsync(ApiUrl, httpContent).Result.Content.ReadAsStringAsync().Result;
+            Debug.WriteLine("response from server: " + responseContent);
+
+            Member resMember = JsonConvert.DeserializeObject<Member>(responseContent);
+            JObject resJObject = JObject.Parse(responseContent);
+            Debug.WriteLine(resJObject["email"]);
+        }
+    }
+}
